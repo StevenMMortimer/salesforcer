1ff2d792f661d67348a6caec8fa6bc71324c3631
Steven Mortimer <reportmort@gmail.com>
2018-03-07 08:39:44 -0500

    Adding Package Skeleton Elements

---
+++ R/salesforcer.R
@@ -0,0 +1,14 @@
+#' \code{salesforcer} package
+#'
+#' An R package connecting to Salesforce APIs using tidy principles
+#'
+#' A package that connects to Salesforce via REST, SOAP, Bulk, and Metadata APIs 
+#' and emphasizes the use of tidy data principles and the tidyverse.
+#' 
+#' Additional material can be found in the 
+#' \href{https://github.com/reportmort/salesforcer}{README} on GitHub
+#'
+#' @docType package
+#' @name salesforcer
+#' @importFrom dplyr %>%
+NULL
