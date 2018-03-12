484ded8947224bd17368de7c81316ca5388bbb80
Steven Mortimer <reportmort@gmail.com>
2018-03-12 02:32:47 -0400

    Try Specifying Path for Travis

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,7 +4,7 @@ library(salesforcer)
 if (identical(tolower(Sys.getenv("NOT_CRAN")), "true") & 
     identical(tolower(Sys.getenv("TRAVIS_PULL_REQUEST")), "false")) {
   
-  salesforcer_test_settings <- readRDS("salesforcer_test_settings.rds")
-  salesforcer_token <- readRDS("salesforcer_token.rds")
+  salesforcer_test_settings <- readRDS("./tests/testthat/salesforcer_test_settings.rds")
+  salesforcer_token <- readRDS("./tests/testthat/salesforcer_token.rds")
   test_check("salesforcer")
 }
